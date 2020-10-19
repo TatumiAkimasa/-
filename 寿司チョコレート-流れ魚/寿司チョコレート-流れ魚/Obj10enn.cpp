3f2dabafeff119f0225e0f1c2885a793c0eec0d8
浩志 芝辻 <h1202016008@hiratagakuen.onmicrosoft.com>
2020-10-19 10:35:01 +0900

    10円を降らせる処理を追加

---
+++ 寿司チョコレート-流れ魚/寿司チョコレート-流れ魚/Obj10enn.cpp
@@ -0,0 +1,81 @@
+//使用するヘッダーファイル
+#include "GameL\DrawTexture.h"
+#include "GameL\HitBoxManager.h"
+
+#include "GameHead.h"
+#include "Obj10enn.h"
+
+//使用するネームスペース
+using namespace GameL;
+
+//コンストラクタ
+CObj10enn::CObj10enn(float x, float y)
+{
+	m_x = x;
+	m_y = y;
+}
+
+//イニシャライズ
+void CObj10enn::Init()
+{
+	m_vy = 0;
+
+	//当たり判定用Hitboxを作成
+	Hits::SetHitBox(this, m_x, m_y, 64, 64, ELEMENT_ENEMY, OBJ_10ENN, 1);
+}
+
+//アクション
+void CObj10enn::Action()
+{
+	m_vy = 0;
+	m_vy += 5.0f;
+
+	//画面外に出たら10円を削除し魚力を加算
+	if (m_y > 600.0f)
+	{
+		this->SetStatus(false);
+		//落下速度加速処理
+		
+		//魚力加算処理
+	}
+
+	m_y += m_vy;
+
+	//HitBoxの内容を更新
+	CHitBox* hit = Hits::GetHitBox(this);	//作成したHitBox更新用の入り口を取り出す
+	hit->SetPos(m_x, m_y);					//入り口から新しい位置(主人公機の位置)情報に置き換える
+	
+	//画面外に出たらHitBoxを削除
+	if (m_y > 600.0f)
+	{
+		this->SetStatus(false);
+		Hits::DeleteHitBox(this);
+	}
+
+	//主人公オブジェクトと接触したら10円を削除
+	if (hit->CheckObjNameHit(ELEMENT_PLAYER) != nullptr)
+	{
+		this->SetStatus(false);		//自身に削除命令を出す。
+		Hits::DeleteHitBox(this);	//10円が所有するHitBoxを削除する
+	}
+}
+
+//ドロー
+void CObj10enn::Draw()
+{
+	float c[4] = { 1.0f,1.0f,1.0f,1.0f };
+	RECT_F src;
+	RECT_F dst;
+
+	src.m_top = 0.0f;
+	src.m_left = 0.0f;
+	src.m_right = 768.0f;
+	src.m_bottom = 768.0f;
+
+	dst.m_top = 0.0f + m_y;
+	dst.m_left = 0.0f + m_x;
+	dst.m_right = 64.0f + m_x;
+	dst.m_bottom = 64.0f + m_y;
+
+	Draw::Draw(0, &src, &dst, c, 0.0f);
+}
---
+++ 寿司チョコレート-流れ魚/寿司チョコレート-流れ魚/Obj10enn.h
@@ -0,0 +1,20 @@
+#pragma once
+//使用するヘッダー
+#include "GameL\SceneObjManager.h"
+//使用するネームスペース
+using namespace GameL;
+
+//オブジェクト：10円
+class CObj10enn : public CObj
+{
+	public:
+		CObj10enn(float x, float y) ;
+		~CObj10enn() {};
+		void Init();		//イニシャライズ
+		void Action();		//アクション
+		void Draw();		//ドロー
+	private:
+		float m_x;			//10円の位置
+		float m_y;
+		float m_vy;			//10円の落下
+};
