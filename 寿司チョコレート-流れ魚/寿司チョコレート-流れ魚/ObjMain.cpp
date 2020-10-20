b422938683a341648571285c4967663d1b693de5
航介 佐藤 <h1202019007@hiratagakuen.onmicrosoft.com>
2020-10-20 11:26:17 +0900

    タイムの追加

--- 寿司チョコレート-流れ魚/寿司チョコレート-流れ魚/ObjMain.cpp
+++ 寿司チョコレート-流れ魚/寿司チョコレート-流れ魚/ObjMain.cpp
@@ -14,19 +14,37 @@ using namespace GameL;
 //イニシャライズ
 void CObjMain::Init()
 {
-
+	f = 0;
+	s_time = 0;
+	m_time = 0;
 }
 
 //アクション
 void CObjMain::Action()
 {
-	
+	f++;
+	//fが60回回ると1秒カウント
+	if (f == 60)
+	{
+		f = 0;
+		s_time++;
+	}
+	//s_timeが60で1分です
+	if (s_time == 60)
+	{
+		s_time = 0;
+		m_time++;
+	}
 }
 
 //ドロー
 void CObjMain::Draw()
 {
-	
+	float c[4] = { 1,1,1,1 };
 
+	//仮マウス位置表示
+	wchar_t str[256];
+	swprintf_s(str, L"タイム　%d:%d", m_time, s_time);
+	Font::StrDraw(str, 20, 40, 40, c);
 
 }
