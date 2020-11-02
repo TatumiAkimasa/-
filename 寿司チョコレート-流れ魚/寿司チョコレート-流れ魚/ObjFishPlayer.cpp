1b7627357ad18668f496558b0c4bbb38fa289882
康太 森井 <h1202019016@hiratagakuen.onmicrosoft.com>
2020-11-02 12:12:47 +0900

    sinisou

--- 寿司チョコレート-流れ魚/寿司チョコレート-流れ魚/ObjFishPlayer.cpp
+++ 寿司チョコレート-流れ魚/寿司チョコレート-流れ魚/ObjFishPlayer.cpp
@@ -134,11 +134,11 @@ void CObjFishPlayer::Draw()
     src.m_right = 828.0f * AniData[m_ani_frame];
     src.m_bottom = 1792.0f;
 
-    dst.m_top = 0.0f + m_py;
-    dst.m_left = 0.0f + m_px;
-    dst.m_right = 64.0f + dst.m_left;
-    dst.m_bottom = 128.0f + dst.m_top;
+    dst.m_top = -16.0f + m_py;
+    dst.m_left = -16.0f + m_px;
+    dst.m_right = 96.0f + dst.m_left;
+    dst.m_bottom = 192.0f + dst.m_top;
 
-    Draw::Draw(11, &src, &dst, c, 0.0f);
+    Draw::Draw(2, &src, &dst, c, 0.0f);
 
 }
