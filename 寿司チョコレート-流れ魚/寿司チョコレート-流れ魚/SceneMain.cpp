0e877e56a3d3c5d6afd40e117c53294a0453ec49
颯人 永原 <h1202016002@hiratagakuen.onmicrosoft.com>
2020-11-24 09:14:18 +0900

    マージ

--- 寿司チョコレート-流れ魚/寿司チョコレート-流れ魚/SceneMain.cpp
+++ 寿司チョコレート-流れ魚/寿司チョコレート-流れ魚/SceneMain.cpp
@@ -168,26 +168,27 @@ void CSceneMain::Scene()
 	m_time++;
 
 	//BGM変更
-	if (((UserData*)Save::GetData())->sp >= 6.0f && bgm_flag == false)
+	if (((UserData*)Save::GetData())->sp >= 10.0f && bgm_flag == false)
 	{
 		Audio::Stop(0);
 		Audio::Start(1);
 		bgm_flag = true;
 	}
-	else if (((UserData*)Save::GetData())->sp < 6.0f && bgm_flag == true)
+	 if (((UserData*)Save::GetData())->sp < 10.0f && bgm_flag == true)
 	{
 		Audio::Stop(1);
 		Audio::Start(0);
 		bgm_flag = false;
 		
 	}
-	else if (((UserData*)Save::GetData())->sp >= 7.0f && bgm_flag2 == false)
+	 //BGM変更（第二段階）
+	if (((UserData*)Save::GetData())->sp >= 18.0f && bgm_flag2 == false)
 	{
 		Audio::Stop(1);
 		Audio::Start(13);
 		bgm_flag2 = true;
 	}
-	else if (((UserData*)Save::GetData())->sp < 7.0f && bgm_flag2 == true)
+	if (((UserData*)Save::GetData())->sp < 18.0f && bgm_flag2 == true)
 	{
 		Audio::Stop(13);
 		Audio::Start(1);
