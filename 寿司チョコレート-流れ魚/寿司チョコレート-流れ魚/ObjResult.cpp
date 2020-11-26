755ca862d9ee0b381dd33302c74fd599cfb4f291
浩志 芝辻 <h1202016008@hiratagakuen.onmicrosoft.com>
2020-11-26 13:57:31 +0900

    リザルト画面のコメント編集

--- 寿司チョコレート-流れ魚/寿司チョコレート-流れ魚/ObjResult.cpp
+++ 寿司チョコレート-流れ魚/寿司チョコレート-流れ魚/ObjResult.cpp
@@ -48,102 +48,147 @@ void CObjResult::Draw()
 	//魚力 0
 	if (((UserData*)Save::GetData())->save_score == 0)
 	{
-		Font::StrDraw(L"魚の風上にも置けない…", 150, 400, 48, c);
+		Font::StrDraw(L"魚の風上にも置けない…", 150, COMMENT_POS, COMMENT_SIZE, c);
 	}
 	//魚力 0～500
 	else if (((UserData*)Save::GetData())->save_score > 0 && ((UserData*)Save::GetData())->save_score < 500)
 	{
-		Font::StrDraw(L"稚魚", 370, 400, 48, c);
+		Font::StrDraw(L"稚魚", 370, COMMENT_POS, COMMENT_SIZE, c);
 	}
 	//魚力 500～1000
 	else if (((UserData*)Save::GetData())->save_score >= 500 && ((UserData*)Save::GetData())->save_score < 1000)
 	{
-		Font::StrDraw(L"もっとがんばろう！", 200, 400, 48, c);
+		Font::StrDraw(L"もっとがんばろう！", 200, COMMENT_POS, COMMENT_SIZE, c);
 	}
 	//魚力 1000～2500
 	else if (((UserData*)Save::GetData())->save_score >= 1000 && ((UserData*)Save::GetData())->save_score < 2500)
 	{
-		Font::StrDraw(L"命とは儚いものだね", 190, 400, 48, c);
+		Font::StrDraw(L"命とは儚いものだね", 190, COMMENT_POS, COMMENT_SIZE, c);
 	}
 	//魚力 2500～5000
 	else if (((UserData*)Save::GetData())->save_score >= 2500 && ((UserData*)Save::GetData())->save_score < 5000)
 	{
-		Font::StrDraw(L"成魚", 370, 400, 48, c);
+		Font::StrDraw(L"成魚", 370, COMMENT_POS, COMMENT_SIZE, c);
 	}
 	//魚力 5000～7500
 	else if (((UserData*)Save::GetData())->save_score >= 5000 && ((UserData*)Save::GetData())->save_score < 7500)
 	{
-		Font::StrDraw(L"まるで魚", 300, 400, 48, c);
+		Font::StrDraw(L"まるで魚", 300, COMMENT_POS, COMMENT_SIZE, c);
 	}
 	//魚力 7500～10000
 	else if (((UserData*)Save::GetData())->save_score >= 7500 && ((UserData*)Save::GetData())->save_score < 10000)
 	{
-		Font::StrDraw(L"ぎょぎょぎょ", 270, 400, 48, c);
+		Font::StrDraw(L"ぎょぎょぎょ", 270, COMMENT_POS, COMMENT_SIZE, c);
 	}
 	//魚力 10000～20000
 	else if (((UserData*)Save::GetData())->save_score >= 10000 && ((UserData*)Save::GetData())->save_score < 20000)
 	{
-		Font::StrDraw(L"もはや魚", 300, 400, 48, c);
+		Font::StrDraw(L"もはや魚", 300, COMMENT_POS, COMMENT_SIZE, c);
 	}
 	//魚力 20000～30000
 	else if (((UserData*)Save::GetData())->save_score >= 20000 && ((UserData*)Save::GetData())->save_score < 30000)
 	{
-		Font::StrDraw(L"魚魚魚～♪", 290, 400, 48, c);
+		Font::StrDraw(L"魚魚魚～♪", 290, COMMENT_POS, COMMENT_SIZE, c);
 	}
 	//魚力 30000～40000
 	else if (((UserData*)Save::GetData())->save_score >= 30000 && ((UserData*)Save::GetData())->save_score < 40000)
 	{
-		Font::StrDraw(L"センスの塊", 290, 400, 48, c);
+		Font::StrDraw(L"センスの塊", 290, COMMENT_POS, COMMENT_SIZE, c);
 	}
 	//魚力 40000～50000
 	else if (((UserData*)Save::GetData())->save_score >= 40000 && ((UserData*)Save::GetData())->save_score < 50000)
 	{
-		Font::StrDraw(L"狂気的にすごい！", 220, 400, 48, c);
+		Font::StrDraw(L"狂気的にすごい！", 220, COMMENT_POS, COMMENT_SIZE, c);
 	}
 	//魚力 50000～60000
 	else if (((UserData*)Save::GetData())->save_score >= 50000 && ((UserData*)Save::GetData())->save_score < 60000)
 	{
-		Font::StrDraw(L"魚とは", 320, 400, 48, c);
+		Font::StrDraw(L"魚とは", 320, COMMENT_POS, COMMENT_SIZE, c);
 	}
 	//魚力 60000～70000
 	else if (((UserData*)Save::GetData())->save_score >= 60000 && ((UserData*)Save::GetData())->save_score < 70000)
 	{
-		Font::StrDraw(L"もはやサメ", 290, 400, 48, c);
+		Font::StrDraw(L"もはやサメ", 290, COMMENT_POS, COMMENT_SIZE, c);
 	}
 	//魚力 70000～80000
 	else if (((UserData*)Save::GetData())->save_score >= 70000 && ((UserData*)Save::GetData())->save_score < 80000)
 	{
-		Font::StrDraw(L"もはや戦艦", 290, 400, 48, c);
+		Font::StrDraw(L"もはや戦艦", 290, COMMENT_POS, COMMENT_SIZE, c);
 	}
 	//魚力 80000～90000
 	else if (((UserData*)Save::GetData())->save_score >= 80000 && ((UserData*)Save::GetData())->save_score < 90000)
 	{
-		Font::StrDraw(L"魚の王", 320, 400, 48, c);
+		Font::StrDraw(L"魚の王", 320, COMMENT_POS, COMMENT_SIZE, c);
 	}
 	//魚力 90000～100000
 	else if (((UserData*)Save::GetData())->save_score >= 90000 && ((UserData*)Save::GetData())->save_score < 100000)
 	{
-		Font::StrDraw(L"制作者の人ですか？", 190, 400, 48, c);
+		Font::StrDraw(L"制作者の人ですか？", 190, COMMENT_POS, COMMENT_SIZE, c);
 	}
-	//魚力 100000～250000
-	else if (((UserData*)Save::GetData())->save_score >= 100000 && ((UserData*)Save::GetData())->save_score < 250000)
+	//魚力 100000～110000
+	else if (((UserData*)Save::GetData())->save_score >= 100000 && ((UserData*)Save::GetData())->save_score < 110000)
 	{
-		Font::StrDraw(L"制作者より上手い！", 190, 400, 48, c);
+		Font::StrDraw(L"制作者の人ですね！", 190, COMMENT_POS, COMMENT_SIZE, c);
 	}
-	//魚力 250000～530000
-	else if (((UserData*)Save::GetData())->save_score >= 250000 && ((UserData*)Save::GetData())->save_score < 530000)
+	//魚力 110000～120000
+	else if (((UserData*)Save::GetData())->save_score >= 110000 && ((UserData*)Save::GetData())->save_score < 120000)
 	{
-		Font::StrDraw(L"ギャラクティックフィッシュ", 90, 400, 48, c);
+		Font::StrDraw(L"制作者でも厳しい…", 190, COMMENT_POS, COMMENT_SIZE, c);
+	}
+	//魚力 120000～130000
+	else if (((UserData*)Save::GetData())->save_score >= 120000 && ((UserData*)Save::GetData())->save_score < 130000)
+	{
+		Font::StrDraw(L"制作者より上手い！", 190, COMMENT_POS, COMMENT_SIZE, c);
+	}
+	//魚力 130000～140000
+	else if (((UserData*)Save::GetData())->save_score >= 130000 && ((UserData*)Save::GetData())->save_score < 140000)
+	{
+		Font::StrDraw(L"制作者でも無理……", 190, COMMENT_POS, COMMENT_SIZE, c);
+	}
+	//魚力 140000～150000
+	else if (((UserData*)Save::GetData())->save_score >= 140000 && ((UserData*)Save::GetData())->save_score < 150000)
+	{
+		Font::StrDraw(L"三倍役満", 190, COMMENT_POS, COMMENT_SIZE, c);
+	}
+	//魚力 150000～160000
+	else if (((UserData*)Save::GetData())->save_score >= 150000 && ((UserData*)Save::GetData())->save_score < 160000)
+	{
+		Font::StrDraw(L"しゃかにゃ", 290, COMMENT_POS, COMMENT_SIZE, c);
+	}
+	//魚力 160000～170000
+	else if (((UserData*)Save::GetData())->save_score >= 160000 && ((UserData*)Save::GetData())->save_score < 170000)
+	{
+		Font::StrDraw(L"初任給16万はちょっと…", 190, COMMENT_POS, COMMENT_SIZE, c);
+	}
+	//魚力 170000～180000
+	else if (((UserData*)Save::GetData())->save_score >= 170000 && ((UserData*)Save::GetData())->save_score < 180000)
+	{
+		Font::StrDraw(L"実は制作者全員彼女いません", 90, COMMENT_POS, COMMENT_SIZE, c);
+	}
+	//魚力 180000～190000
+	else if (((UserData*)Save::GetData())->save_score >= 180000 && ((UserData*)Save::GetData())->save_score < 190000)
+	{
+		Font::StrDraw(L"サケの切り身、実は生なんだ", 90, COMMENT_POS, COMMENT_SIZE, c);
+	}
+	//魚力 190000～200000
+	else if (((UserData*)Save::GetData())->save_score >= 190000 && ((UserData*)Save::GetData())->save_score < 200000)
+	{
+		Font::StrDraw(L"やりこんでくれてありがとう", 90, COMMENT_POS, COMMENT_SIZE, c);
+	}
+	//魚力 200000～530000
+	else if (((UserData*)Save::GetData())->save_score >= 200000 && ((UserData*)Save::GetData())->save_score < 530000)
+	{
+		Font::StrDraw(L"ギャラクティックフィッシュ", 90, COMMENT_POS, COMMENT_SIZE, c);
 	}
 	//魚力 530000～1000000
 	else if (((UserData*)Save::GetData())->save_score >= 530000 && ((UserData*)Save::GetData())->save_score < 1000000)
 	{
-		Font::StrDraw(L"魚力53万越えだと", 210, 400, 48, c);
+		Font::StrDraw(L"魚力53万越えだと", 210, COMMENT_POS, COMMENT_SIZE, c);
 	}
 	//魚力 1000000～
 	else if (((UserData*)Save::GetData())->save_score >= 1000000)
 	{
-		Font::StrDraw(L"チートが検出されました", 140, 400, 48, c);
+		Font::StrDraw(L"チートが検出されました", 140, COMMENT_POS, COMMENT_SIZE, c);
 	}
 
 	//タイム表示
