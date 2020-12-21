0e5adcb8a82602f01c55ffbd71cf6e23397ec39c
航介 佐藤 <h1202019007@hiratagakuen.onmicrosoft.com>
2020-12-21 12:02:52 +0900

    シーンセッティングの作成

---
+++ 寿司チョコレート-流れ魚/寿司チョコレート-流れ魚/SceneSetting.cpp
@@ -0,0 +1,36 @@
+//STLデバッグ機能をオフにする
+#define _SECURESCL (0)
+#define _HAS_ITERATOR_DEBUGGING (0)
+
+//GameLで使用するヘッダー
+#include "GameL\SceneObjManager.h"
+#include "GameL\DrawFont.h"
+#include "GameL\Audio.h"
+#include"GameL/DrawTexture.h"
+
+//使用するゲームスペース
+using namespace GameL;
+
+//使用するヘッダー
+#include "SceneSetting.h"
+#include "GameHead.h"
+
+
+//コンストラクタ
+CSceneSetting::CSceneSetting()
+{
+
+}
+
+//ゲームメイン初期化メソッド
+void CSceneSetting::InitScene()
+{
+	CObjSetting* cs = new CObjSetting();//実績ブジェクト作成
+	Objs::InsertObj(cs, OBJ_SETTING, 10);//実績オブジェクト登録
+}
+
+//ゲームメイン実行中メソッド
+void CSceneSetting::Scene()
+{
+
+}
